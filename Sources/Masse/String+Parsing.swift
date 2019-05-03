f056f6ab8ced636c3b8b1976e140e0d7ec6637e6
Benedikt Terhechte <terhechte@gmail.com>
2019-05-03 21:36:46 +0200

    Update String+Parsing.swift

--- Sources/Masse/String+Parsing.swift
+++ Sources/Masse/String+Parsing.swift
@@ -1,4 +1,10 @@
 extension String {
+    /// Retrieve the contents between two strings. This is used, for example, 
+    /// to get template block names, such as the `hello-world` in:
+    /// `{{BEGIN name="hello-world"}}`
+    /// - parameter beginString: The required prefix before the to-be-retrieved-string
+    /// - parameter endString: The required suffix before the to-be-retrieved-string
+    /// Only returns a valid string if both prefix and suffix exist
     func between(beginString: String, endString: String) -> String? {
         // why is it still such a pain to grab a range out of string in swift...
         // I know I could use `NSRange` and then convert it, but that feels like cheating
